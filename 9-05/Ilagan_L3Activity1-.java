b7c299792197999295ed77dd71a780440a5c7fee
maelaiza <elaiza.ilagan@awsys-i.com>
2024-11-05 11:29:51 +0800

    9-03 activity

---
+++ 9-05/Ilagan_L3Activity1-.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package Ilagan_Activity;
+
+/**
+ * 
+ */
+public class Ilagan_L3Activity1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 String name= "Elaiza Ilagan";
+		 String country="Philippines";
+		 String sport="running";
+		 String game="pokemongo";
+		 String subject="Math";
+		 
+		int age= 23;
+		int hours= 1;
+		int score= 99;
+		
+	System.out.println("My name is " + name + ". I'm " + age + " years old from "+ country + 
+			".\n My favorite sport is " + sport + ". I run for " + hours + " hour "+" every weekend."
+			+ "\n I like to play " + game +" when I'm tired. "
+			+ "\n In school, my favorite subject was " + subject +" and Recess. "
+			+ "\n I scored a " + score);
+			
+		
+		
+		
+
+	}
+
+}
